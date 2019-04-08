95b28e3c1a288951ec6e73abb41cbe7eccdf6545
yasu <yasu@DESKTOP-3GLE2PD>
2019-04-08 16:20:40 +0900

    ちょい

--- visualizer/src/tmcit/yasu/game/GameData.java
+++ visualizer/src/tmcit/yasu/game/GameData.java
@@ -4,7 +4,27 @@ import java.awt.Point;
 import java.util.ArrayList;
 
 public class GameData {
-	private int nowTurn, mapWidth, mapHeight;
-	private int[][] mapScore, territoryMap;
+	private int maxTurn, mapWidth, mapHeight;
+	private int[][] mapScore;
 	private ArrayList<Point> myPlayers, rivalPlayers;
+	
+	public int getMaxTurn() {
+		return maxTurn;
+	}
+	public int getMapWidth() {
+		return mapWidth;
+	}
+	public int getMapHeight() {
+		return mapHeight;
+	}
+	public int[][] getMapScore() {
+		return mapScore;
+	}
+	public ArrayList<Point> getMyPlayers() {
+		return myPlayers;
+	}
+	public ArrayList<Point> getRivalPlayers() {
+		return rivalPlayers;
+	}
+	
 }
--- visualizer/src/tmcit/yasu/game/GameMaster.java
+++ visualizer/src/tmcit/yasu/game/GameMaster.java
@@ -1,5 +1,34 @@
 package tmcit.yasu.game;
 
-public class GameMaster {
+import java.awt.Point;
+import java.util.ArrayList;
 
+import tmcit.yasu.player.Player;
+
+public class GameMaster {
+	private GameData gameData;
+	private Player myPlayer, rivalPlayer;
+	
+	// start game data
+	private int mapWidth, mapHeight, maxTurn;
+	private int[][] mapScore;
+	private ArrayList<Point> startMyPlayers, startRivalPlayers;
+	
+	// now game data
+	
+	
+	public GameMaster(GameData gameData0, Player myPlayer0, Player rivalPlayer0) {
+		gameData = gameData0;
+		myPlayer = myPlayer0;
+		rivalPlayer = rivalPlayer0;
+	}
+	
+	private void init() {
+		mapWidth = gameData.getMapWidth();
+		mapHeight = gameData.getMapHeight();
+		maxTurn = gameData.getMaxTurn();
+		mapScore = gameData.getMapScore();
+		startMyPlayers = gameData.getMyPlayers();
+		startRivalPlayers = gameData.getRivalPlayers();
+	}
 }
