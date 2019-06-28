7d2b6401c4f5b6e743fc7fa19cd64bff0de9b711
Lafolia <violgdae@gmail.com>
2019-06-28 17:54:47 +0900

    refactoring base

--- solver/google/base/base_system.cpp
+++ solver/google/base/base_system.cpp
@@ -7,8 +7,8 @@ bool GameData::Input() {
 	cin >> width >> height;
 	cin >> agent_num;
 
-	for (int8_t &&h = 0; h < height; ++h) {
-		for (int8_t &&w = 0; w < width; ++w) {
+	for (int_fast32_t &&h = 0; h < height; ++h) {
+		for (int_fast32_t &&w = 0; w < width; ++w) {
 			cin >> field_data[h][w];
 		}
 	}
@@ -16,13 +16,7 @@ bool GameData::Input() {
 	return true;
 }
 
-const bool GameData::IntoField(const int16_t &pos, const int8_t &way) const {
-	const int8_t &&nh = ((pos >> 5)&kFive) + kNextToEit[way].h;
-	const int8_t &&nw = (pos&kFive) + kNextToEit[way].w;
-	return (0 <= nh && nh < height && 0 <= nw && nw < width);
-}
-
-const bool GameData::IntoField(const Position &pos) const {
+bool GameData::IntoField(const Position &pos) {
 	return (0 <= pos.h && pos.h < height && 0 <= pos.w && pos.w < width);
 }
 
@@ -31,38 +25,30 @@ bool TurnData::Input(const GameData &game_data) {
 	if (this->now_turn > game_data.max_turn)
 		return false;
 
-	int8_t &&tile = 0;
-	for (int8_t &&h = 0; h < game_data.height; ++h) {
-		for (int8_t &&w = 0; w < game_data.width; ++w) {
-			cin >> tile;
-			tile_data[h] -= (tile_data[h] >> (w*2)) & kTwo;
-			tile_data[h] += tile<<(w*2);
+	for (int_fast32_t &&h = 0; h < game_data.height; ++h) {
+		for (int_fast32_t &&w = 0; w < game_data.width; ++w) {
+			cin >> tile_data[h][w];
 		}
 	}
 
-	int8_t &&hpos = 0, &&wpos = 0;
 	for (int8_t &&team_id = 0; team_id < 2; ++team_id) {
 		for (int8_t agent_id = 0; agent_id < game_data.agent_num; ++agent_id) {
-			cin >> wpos >> hpos;
-			agents_position[team_id][agent_id] = (hpos << 5) + wpos;
+			cin >> agents_position[team_id][agent_id].w
+				>> agents_position[team_id][agent_id].h;
 		}
 	}
 
 	return true;
 }
 
-const int8_t TurnData::GetTileState(const int8_t &h, const int8_t &w) const {
-	return (tile_data[h]>>(w*2))&kTwo;
+int_fast32_t& TurnData::GetTileState(const int_fast32_t &h, const int_fast32_t &w) {
+	return tile_data[h][w];
 }
 
-const int8_t TurnData::GetTileState(const Position &pos) const {
+int_fast32_t& TurnData::GetTileState(const Position &pos) {
 	return GetTileState(pos.h, pos.w);
 }
 
-const Position TurnData::GetPosition(const int8_t &team_id, const int8_t &agent_id) const {
-	const int16_t &&num = static_cast<int16_t&&>(agents_position[team_id][agent_id]);
-	const int8_t &&hpos = (num >> 5)&kFive;
-	const int8_t &&wpos = num&kFive;
-
-	return Position(hpos, wpos);
+Position& TurnData::GetPosition(const int_fast32_t &team_id, const int_fast32_t &agent_id) {
+	return agents_position[team_id][agent_id];
 }
--- solver/google/base/base_system.h
+++ solver/google/base/base_system.h
@@ -7,11 +7,11 @@
 using namespace std;
 
 struct Position {
-	int8_t h = {};
-	int8_t w = {};
+	int_fast32_t h = {};
+	int_fast32_t w = {};
 
 	constexpr Position() {};
-	constexpr Position(const &h, const &w) :
+	constexpr Position(const int_fast32_t &h, const int_fast32_t &w) :
 		h(h),
 		w(w)
 	{};
@@ -25,55 +25,44 @@ struct Position {
 };
 
 struct GameData {
-	int32_t max_turn = {};
-	int32_t height = {};
-	int32_t width = {};
-	int32_t agent_num = {};
-	array<array<int8_t, 20>, 20> field_data = {};
+	int_fast32_t max_turn = {};
+	int_fast32_t height = {};
+	int_fast32_t width = {};
+	int_fast32_t agent_num = {};
+	array<array<int_fast32_t, 20>, 20> field_data = {};
 
-	constexpr GameData() {};
+	constexpr GameData() noexcept {};
 
 	bool Input();
-	const bool IntoField(const int16_t&, const int8_t&) const;
-	const bool IntoField(const Position&) const;
+	bool IntoField(const Position&);
 };
 
 struct TurnData {
-	int8_t now_turn = {};
-	int8_t agent_num = {};
-	int32_t tile_point = {};
-	int32_t area_point = {};
-	array<array<int16_t, 8>, 2> agents_position = { 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999,
-													9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999};
-	array<int64_t, 20> tile_data = {};
-	array<int32_t, 20> is_area = {};					// あるマスが領域であるか
+	int_fast32_t now_turn = {};
+	int_fast32_t agent_num = {};
+	int_fast32_t tile_point = {};
+	int_fast32_t area_point = {};
+	array<array<Position, 8>, 2> agents_position = {};
+	array<array<int_fast32_t, 20>, 20> tile_data = {};
+	array<array<int_fast32_t, 20>, 20> is_area = {};					// あるマスが領域であるか
 
 	constexpr TurnData() {};
 
 	bool Input(const GameData&);
-	const int8_t GetTileState(int8_t &h, int8_t &w) const;
-	const int8_t GetTileState(const Position&) const;
-	const Position GetPosition(const int8_t&, const int8_t&) const;
-	void CalculationAllTilePoint(const GameData&, const int8_t&);
-	void CalculationTilePoint(const GameData&, const TurnData&, const int8_t&);
-	void CalculationAllAreaPoint(const GameData&, const int8_t&);
-	void CalculationAreaPoint(const GameData&, const int8_t&);
+	int_fast32_t& GetTileState(const int_fast32_t &h, const int_fast32_t &w);
+	int_fast32_t& GetTileState(const Position&);
+	Position& GetPosition(const int_fast32_t&, const int_fast32_t&);
+	void CalculationTilePoint(const GameData&, const TurnData&, const int_fast32_t&);
+	void CalculationAllTilePoint(const GameData&, const int_fast32_t&);
+	void CalculationAreaPoint(const GameData&, const int_fast32_t&);
+	void CalculationAllAreaPoint(const GameData&, const int_fast32_t&);
+	void Transition(const int_fast32_t&);
 };
-
 
 enum TileColors {
 	kAlly = 0,
 	kRival = 1,
 	kBrank = 2
-};
-
-// nビットの値を取る用。 target&kNumで下位nビットの値を取れます
-enum BitAnds {
-	kOne = (1<<1) - 1,
-	kTwo = (1<<2) - 1,
-	kThree = (1<<3) - 1,
-	kFour = (1<<4) - 1,
-	kFive = (1<<5) - 1
 };
 
 // 四近傍
