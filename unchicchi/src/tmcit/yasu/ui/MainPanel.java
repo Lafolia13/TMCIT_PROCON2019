d66ff782ba9b56cdc1f1781a001af02afb780ba5
yasu89 <19990967+yasu89@users.noreply.github.com>
2019-06-28 17:41:50 +0900

    とりあえずなUI

---
+++ unchicchi/src/tmcit/yasu/ui/MainPanel.java
@@ -0,0 +1,16 @@
+package tmcit.yasu.ui;
+
+import javax.swing.JPanel;
+
+public class MainPanel extends JPanel{
+	public MainPanel() {
+	}
+	
+	private void init() {
+		
+	}
+	
+	private void initLayout() {
+		
+	}
+}
