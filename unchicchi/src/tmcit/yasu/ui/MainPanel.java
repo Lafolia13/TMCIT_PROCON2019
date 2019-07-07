892676603e0e0ccc114dafaccc7d8b342aba2d8c
yasu89 <19990967+yasu89@users.noreply.github.com>
2019-07-07 16:38:39 +0900

    エージェント設定のパネル

--- unchicchi/src/tmcit/yasu/ui/MainPanel.java
+++ unchicchi/src/tmcit/yasu/ui/MainPanel.java
@@ -4,21 +4,21 @@ import javax.swing.JPanel;
 
 public class MainPanel extends JPanel{
 	private SettingPanel settingPanel;
-	
+
 	public MainPanel() {
 		init();
 		initLayout();
 	}
-	
+
 	private void init() {
 		settingPanel = new SettingPanel();
 	}
-	
+
 	private void initLayout() {
 		setLayout(null);
-		
-		settingPanel.setBounds(10, 10, 300, 400);
-		
+
+		settingPanel.setBounds(10, 10, 400, 400);
+
 		add(settingPanel);
 	}
 }
--- unchicchi/src/tmcit/yasu/ui/SettingPanel.java
+++ unchicchi/src/tmcit/yasu/ui/SettingPanel.java
@@ -7,25 +7,31 @@ import tmcit.yasu.util.Constant;
 
 public class SettingPanel extends JPanel{
 	private JLabel nameLabel;
-	
+
+	private AgentSelectPanel agentSelectPanel;
+
 	public SettingPanel() {
 		init();
 		initLayout();
 	}
-	
+
 	private void init() {
 		setBorder(Constant.DEFAULT_LINE_BORDER);
-		
+
 		nameLabel = new JLabel("ê›íË");
 		nameLabel.setFont(Constant.DEFAULT_FONT);
+
+		agentSelectPanel = new AgentSelectPanel();
 	}
-	
+
 	private void initLayout() {
 		setLayout(null);
-		
+
 		nameLabel.setBounds(10, 5, 100, 30);
-		
+		agentSelectPanel.setBounds(10, 40, 300, 350);
+
 		add(nameLabel);
+		add(agentSelectPanel);
 	}
 }
 
