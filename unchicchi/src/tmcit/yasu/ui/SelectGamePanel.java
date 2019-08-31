34369d50ff3da233e21e033e0c40f56b673627bf
yasu89 <19990967+yasu89@users.noreply.github.com>
2019-08-31 18:25:15 +0900

    ゲーム一覧の取得

---
+++ unchicchi/src/tmcit/yasu/ui/SelectGamePanel.java
@@ -0,0 +1,49 @@
+package tmcit.yasu.ui;
+
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+import tmcit.yasu.data.ConnectSetting;
+import tmcit.yasu.data.Field;
+import tmcit.yasu.data.MatchesData;
+import tmcit.yasu.exception.InvalidMatchesException;
+import tmcit.yasu.exception.InvalidTokenException;
+import tmcit.yasu.exception.TooEarlyException;
+import tmcit.yasu.util.Network;
+
+public class SelectGamePanel extends JPanel{
+	private ConnectSetting connectSetting;
+	private ArrayList<MatchesData> matchesData;
+	private ArrayList<Integer> startTime;
+
+	public SelectGamePanel(ConnectSetting connectSetting0) {
+		connectSetting = connectSetting0;
+
+		getMatchesData();
+	}
+
+	private void getMatchesData() {
+		Network net = new Network(connectSetting.url, connectSetting.port, connectSetting.token);
+		startTime = new ArrayList<Integer>();
+
+		try {
+			matchesData = net.getMatches();
+		} catch (InvalidTokenException e) {
+			e.printStackTrace();
+		}
+
+		for(MatchesData nowMatchData : matchesData) {
+			try {
+				Field nowField = net.getMatcheStatus(nowMatchData.id);
+				startTime.add(nowField.startedAtUnixTime);
+			} catch (InvalidTokenException e) {
+				startTime.add(0);
+			} catch (InvalidMatchesException e) {
+				startTime.add(e.startUnixTime);
+			} catch (TooEarlyException e) {
+				startTime.add(e.startUnixTime);
+			}
+		}
+	}
+}
