c2ef7551d104b782a990db8289a669cfdd50d83a
yasu89 <19990967+yasu89@users.noreply.github.com>
2019-09-02 13:30:48 +0900

    リファクタ

--- unchicchi/src/tmcit/yasu/util/GameNetworkRunnable.java
+++ unchicchi/src/tmcit/yasu/util/GameNetworkRunnable.java
@@ -13,19 +13,21 @@ public class GameNetworkRunnable implements Runnable{
 	private ConnectSetting connectSetting;
 	private MatchesData matchData;
 	private GameStatusPanel gameStatusPanel;
+	
+	// 次にpingを送信する時間
+	private long nextPingUnixTime, nowUnixTime, gameStartUnixTime;
 
 	public GameNetworkRunnable(ConnectSetting connectSetting0, MatchesData matchData0, GameStatusPanel gameStatusPanel0) {
 		connectSetting = connectSetting0;
 		matchData = matchData0;
 		gameStatusPanel = gameStatusPanel0;
+		
+		nextPingUnixTime = 0;
+		gameStartUnixTime = -1;
 	}
-
-	@Override
-	public void run() {
-		Network net = new Network(connectSetting.url, connectSetting.port, connectSetting.token);
-
-		// 最初にゲームが開始しているか確認
-		long startUnixTime = -1;
+	
+	// ゲームの状態を確認
+	private void checkGameStatus(Network net) {
 		try {
 			net.getMatcheStatus(matchData.id);
 			gameStatusPanel.changeGameStatus("ゲーム中");
@@ -34,36 +36,52 @@ public class GameNetworkRunnable implements Runnable{
 		} catch (InvalidMatchesException e2) {
 			gameStatusPanel.changeGameStatus("参加できません");
 		} catch (TooEarlyException e2) {
-			startUnixTime = e2.startUnixTime;
-			long nowUnixTime = System.currentTimeMillis() / 1000L;
-			long lastTime = startUnixTime - nowUnixTime;
+			gameStartUnixTime = e2.startUnixTime;
+			nowUnixTime = System.currentTimeMillis() / 1000L;
+			long lastTime = gameStartUnixTime - nowUnixTime;
 			gameStatusPanel.changeGameStatus("開始前(残り" + String.valueOf(lastTime) + "秒)");
 		}
+	}
+	
+	// pingでサーバとの接続状態を取得
+	private void checkPing(Network net) {
+		try {
+			boolean pingResult = net.ping();
+			if(pingResult) {
+				gameStatusPanel.changeServerStatus("正常");
+			}else {
+				gameStatusPanel.changeServerStatus("失敗");
+			}
+		} catch (IOException e1) {
+			gameStatusPanel.changeServerStatus("通信不能");
+		} catch (InvalidTokenException e1) {
+			gameStatusPanel.changeServerStatus("トークンエラー");
+		}
+	}
+
+	@Override
+	public void run() {
+		Network net = new Network(connectSetting.url, connectSetting.port, connectSetting.token);
+
+		// 最初にゲームが開始しているか確認
+		checkGameStatus(net);
 
 		while(true) {
-			// pingでサーバとの接続状態を取得
-			try {
-				boolean pingResult = net.ping();
-				if(pingResult) {
-					gameStatusPanel.changeServerStatus("正常");
-				}else {
-					gameStatusPanel.changeServerStatus("失敗");
-				}
-			} catch (IOException e1) {
-				gameStatusPanel.changeServerStatus("通信不能");
-			} catch (InvalidTokenException e1) {
-				gameStatusPanel.changeServerStatus("トークンエラー");
-			}
-
-			// ゲームが開始していない場合
 			long nowUnixTime = System.currentTimeMillis() / 1000L;
-			if(startUnixTime - nowUnixTime > 0) {
-				gameStatusPanel.changeGameStatus("開始前(残り" + String.valueOf(startUnixTime - nowUnixTime) + "秒)");
+
+			// pingでサーバとの接続状態を取得
+			if(nextPingUnixTime < nowUnixTime) {
+				checkPing(net);
+				nextPingUnixTime = nowUnixTime + connectSetting.interval;
 			}
 
-			// TODO: 設定の値に合わせる
+			// ゲームが開始していない場合、秒数をカウントダウン
+			if(gameStartUnixTime - nowUnixTime > 0) {
+				gameStatusPanel.changeGameStatus("開始前(残り" + String.valueOf(gameStartUnixTime - nowUnixTime) + "秒)");
+			}
+
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(50);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
