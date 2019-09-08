ed43c6a0a875f93fe7d47d353b567711261a71e8
Lafolia <violgdae@gmail.com>
2019-09-08 16:47:20 +0900

    new branch extends toriaezu solver

---
+++ solver/google/search/split_agent/evaluation.h
@@ -0,0 +1,31 @@
+#ifndef EVALUATION_H
+#define EVALUATION_H
+
+#include "../../calculation/calculation.h"
+
+namespace toriaezu {
+
+const int_fast32_t evaluations_num = 6;
+
+double GetEvaluation(const GameData&, TurnData&, const TurnData&,
+					 const int_fast32_t&, const int_fast32_t&, const double&);
+
+double AllyTilePointDifference(const GameData&, const TurnData&,
+							   const TurnData&, const int_fast32_t&);
+
+double RivalTilePointDifference(const GameData&, const TurnData&,
+								const TurnData&, const int_fast32_t&);
+
+double AllyAreaPointDifference(const GameData&, const TurnData&,
+							   const TurnData&, const int_fast32_t&);
+
+double RivalAreaPointDifference(const GameData&, const TurnData&,
+								const TurnData&, const int_fast32_t&);
+
+double BeforeEvaluationBias(const GameData&, const double&, const int_fast32_t&);
+
+double FirstEvaluation(const GameData&, const double&, const int_fast32_t&);
+
+}
+
+#endif
