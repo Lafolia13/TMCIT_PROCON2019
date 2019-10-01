7c0ec82816d9e6302435ec8d67c41fa09927a3a3
Lafolia <violgdae@gmail.com>
2019-10-01 14:50:27 +0900

    add two evaluations

--- solver/google/search/disperse_agent/evaluation.h
+++ solver/google/search/disperse_agent/evaluation.h
@@ -5,7 +5,7 @@
 
 namespace disperse_agent {
 
-const int_fast32_t evaluations_num = 7;
+const int_fast32_t evaluations_num = 10;
 
 double GetEvaluation(const GameData&, TurnData&, const TurnData&,
 					 const int_fast32_t&, const int_fast32_t&, const double&);
@@ -21,6 +21,11 @@ double AllyAreaPointDifference(const GameData&, const TurnData&,
 
 double RivalAreaPointDifference(const GameData&, const TurnData&,
 								const TurnData&, const int_fast32_t&);
+
+double StayMinusMasu(const GameData&, const TurnData&, const int_fast32_t&);
+
+double ActionToRivalLocation(const GameData&, const TurnData,
+							 const vector<Move>&, const int_fast32_t&);
 
 double DisperseAgent(const GameData&, const TurnData&, const int_fast32_t&);
 
